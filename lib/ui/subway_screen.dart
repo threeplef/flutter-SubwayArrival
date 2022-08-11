492910e4f71fea7f5fcdf6ce1b5b296ffac13c51
minhaneul <onlynforever@gmail.com>
2022-08-11 15:14:48 +0900

    Create: Realtime Seoul Subway Arrival App

---
+++ lib/ui/subway_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SubWayScreen extends StatelessWidget {
+  const SubWayScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
